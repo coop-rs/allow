eef409bfffb5d4f9c97642cdcd9d33e9136efb37
Peter Kehl <peter.kehl@gmail.com>
2023-03-27 09:14:35 -0700

    Minimized compilation problem with std::thread::LocalKey

--- src/lib.rs
+++ src/lib.rs
@@ -1,63 +1,10 @@
-//#![feature(proc_macro_quote)]
-
-//use std::str::FromStr;
-
-use proc_macro::{Delimiter, Group, Ident, Punct, Spacing, Span, TokenStream, TokenTree};
-
-#[proc_macro_attribute]
-pub fn prefix_attrs_before_item(attrs: TokenStream, item: TokenStream) -> TokenStream {
-    //assert!(attrs.is_empty());
-    let streams = [attrs, item];
-    TokenStream::from_iter(streams)
-}
-// ------
-
-fn hash() -> TokenStream {
-    TokenStream::from(TokenTree::Punct(Punct::new('#', Spacing::Alone)))
-}
-
-fn allow() -> TokenTree {
-    TokenTree::Ident(Ident::new("allow", Span::call_site()))
-}
+use proc_macro::{Literal, TokenStream, TokenTree};
 
 thread_local! {
-    static HASH: TokenStream = hash();
-    static ALLOW: TokenTree = allow();
+    static ONE: TokenStream = TokenStream::from(TokenTree::Literal(Literal::i32_unsuffixed(1)));
 }
 
-// https://github.com/rust-lang/rust/issues/29594
-// cfg(target_thread_local)
-
-// ----
-//
-// Actual macros for public use
 #[proc_macro_attribute]
-pub fn unused(given_attrs: TokenStream, item: TokenStream) -> TokenStream {
-    assert!(given_attrs.is_empty());
-    if false {
-        return item;
-    }
-    // The string source for `attrs` is  internal, hence well formed.
-    //let attrs = TokenStream::from_str("#[allow(unused)]").unwrap();
-
-    let hash = TokenStream::from(TokenTree::Punct(Punct::new('#', Spacing::Alone)));
-
-    let allow = TokenTree::Ident(Ident::new("allow", Span::call_site()));
-
-    // BEWARE: If the lint name doesn't exist, then even if the consumer code uses
-    // `#[deny(unknown_lints)]`, that (incorrect lint name) will NOT get reported.
-    let unused_as_stream =
-        TokenStream::from(TokenTree::Ident(Ident::new("unused", Span::call_site())));
-    let parented_unused = TokenTree::Group(Group::new(Delimiter::Parenthesis, unused_as_stream));
-
-    let allow_unused = TokenStream::from_iter([ALLOW.with(Clone::clone), parented_unused]);
-    //let allow_unused = TokenStream::from_iter([allow, parented_unused]);
-
-    let squared = TokenStream::from(TokenTree::Group(Group::new(
-        Delimiter::Bracket,
-        allow_unused,
-    )));
-
-    TokenStream::from_iter([HASH.with(Clone::clone), squared, item])
-    //TokenStream::from_iter([hash, squared, item])
+pub fn unused(_given_attrs: TokenStream, _item: TokenStream) -> TokenStream {
+    TokenStream::from_iter([ONE.with(Clone::clone)])
 }
