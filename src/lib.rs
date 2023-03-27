08f8af3c4ae7a3fc8401fc9080f3ed9598124939
Peter Kehl <peter.kehl@gmail.com>
2023-03-27 11:46:39 -0700

    Minimized compilation problem with thread_local feature (currently only on nightly)

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,17 @@
+#![feature(thread_local)]
+
+use once_cell::unsync::OnceCell;
 use proc_macro::{Literal, TokenStream, TokenTree};
 
-thread_local! {
-    static ONE: TokenStream = TokenStream::from(TokenTree::Literal(Literal::i32_unsuffixed(1)));
-}
+#[thread_local]
+// static ONE: TokenStream = TokenStream::from(...) failed, and rustc was suggesting Lazy::new(...).
+// However, that would involve a Mutex.
+//
+// See https://docs.rs/once_cell/latest/once_cell/unsync/struct.OnceCell.html
+static ONE: OnceCell<TokenStream> = OnceCell::new();
 
 #[proc_macro_attribute]
 pub fn unused(_given_attrs: TokenStream, _item: TokenStream) -> TokenStream {
-    TokenStream::from_iter([ONE.with(Clone::clone)])
+    ONE.get_or_init(|| TokenStream::from(TokenTree::Literal(Literal::i32_unsuffixed(1))))
+        .clone()
 }
--- tests/simple.rs
+++ tests/simple.rs
@@ -1,5 +1,11 @@
 #![feature(stmt_expr_attributes)]
 #![feature(proc_macro_hygiene)]
+
+#[test]
+fn test_unused() {
+    _unused();
+}
+
 fn _unused() -> i32 {
     #[allows::unused]
     0
